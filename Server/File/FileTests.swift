69dfd418a166efa85406914cfef0fc89d5801d50
Steve Streza <steve@stevestreza.com>
2015-07-31 20:06:15 -0700

    Added initial unit testing support

---
+++ Server/File/FileTests.swift
@@ -0,0 +1,55 @@
+//
+//  FileTests.swift
+//  Jelly
+//
+//  Created by Steve Streza on 31.7.15.
+//  Copyright © 2015 Lunar Guard. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Jelly
+
+class FileSpec: QuickSpec {
+	override func spec() {
+		describe("file") {
+			let root = File.rootDirectory
+			it("should have valid root") {
+				expect(root.path) == File.pathSeparator
+				expect(root.exists) == true
+				expect(root.isDirectory) == true
+				expect(root.isFile) == false
+				expect(root.pathComponents) == [File.pathSeparator]
+				expect(root.lastPathComponent) == File.pathSeparator
+			}
+			
+			let etc = root.fileByAppendingPathComponent("etc")
+			it("should have created /etc properly") {
+				expect(etc.path) == "\(File.pathSeparator)etc"
+				expect(etc.exists) == true
+				expect(etc.isDirectory) == true
+				expect(etc.isFile) == false
+				expect(etc.pathComponents) == [File.pathSeparator, "etc"]
+				expect(etc.lastPathComponent) == "etc"
+				expect(etc.parentDirectory) == root
+			}
+			
+			let passwd = etc.fileByAppendingPathComponent("passwd")
+			it("should have created /etc/passwd properly") {
+				expect(passwd.path) == "\(File.pathSeparator)etc\(File.pathSeparator)passwd"
+				expect(passwd.exists) == true
+				expect(passwd.isDirectory) == false
+				expect(passwd.isFile) == true
+				expect(passwd.pathComponents) == [File.pathSeparator, "etc", "passwd"]
+				expect(passwd.lastPathComponent) == "passwd"
+				expect(passwd.parentDirectory) == etc
+			}
+			
+			let passwdData = passwd.data
+			it("should be able to read /etc/passwd data") {
+				expect(passwdData) != nil
+				expect(passwdData!.bytes.count) > 0
+			}
+		}
+	}
+}
---
+++ Server/Process/RuntimeTests.swift
@@ -0,0 +1,27 @@
+//
+//  RuntimeTests.swift
+//  Jelly
+//
+//  Created by Steve Streza on 31.7.15.
+//  Copyright © 2015 Lunar Guard. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Jelly
+
+class RuntimeSpec: QuickSpec {
+	override func spec() {
+		describe("Runtime") {
+			context("environment variables") {
+				it("should have the TestEnvironmentVariable variable") {
+					expect(Process.environment("TestEnvironmentVariable")).to(equal("ItWorks"))
+				}
+
+				it("should not have a TestEnvironmentVariableTwo variable") {
+					expect(Process.environment("TestEnvironmentVariableTwo")).to(beNil())
+				}
+			}
+		}
+	}
+}
